0b7294254d1b886048c8ffd788292c6a2a096952
alemandaluz <miguel_pcm@yahoo.co.uk>
2026-05-16 12:38:13 -0400

    tarde

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,8 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
+import Dealers from './components/Dealers/Dealers';
+
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       {/* Route for the Registration page */}
       <Route path="/register" element={<Register />} />
+      <Route path="/dealers/" element={<Dealers/>} />
+
     </Routes>
   );
 }
